562be7f23c4854a245b3b6beb7707d35d9b87b5e
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-04-26 10:06:52 +0200

    change: add validate and register handlers functions

--- internal/routes/index.routes.go
+++ internal/routes/index.routes.go
@@ -1,9 +1,46 @@
 package routes
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 
+func buildPath(subPath string,identifierPath string) string{
+	return fmt.Sprintf("%v/{%v}",subPath,identifierPath)
+}
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+
+func ValidateHandlers(handlers map[string] func(w http.ResponseWriter, r *http.Request)) bool {
+	isValid := true
+	methods := []string{"CGET","POST","GET","PUT","DELETE"}
+
+	for _,method := range methods {
+		if _, ok := handlers[method]; !ok {
+			handlers[method] = func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusNotImplemented)
+			}
+			isValid = false
+		}
+	}
+	return isValid
+}
+
+
+func RegisterHandlers(handlers map[string] func(w http.ResponseWriter, r *http.Request) , subPath string,identifierPath string, r *mux.Router) {
+
+	var fullPath string = buildPath(subPath,identifierPath)
+
+	if (ValidateHandlers(handlers) == false) {
+		log.Println("Handlers are not valid")
+	}
+	r.HandleFunc(subPath, handlers["CGET"]).Methods("GET")
+	r.HandleFunc(subPath, handlers["POST"]).Methods("POST")
+
+	r.HandleFunc(fullPath, handlers["GET"]).Methods("GET")
+	r.HandleFunc(fullPath, handlers["PUT"]).Methods("PUT")
+	r.HandleFunc(fullPath, handlers["DELETE"]).Methods("DELETE")
 }
