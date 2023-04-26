823a59a56664a85aa598988b903d41e3b4474274
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-04-26 10:07:27 +0200

    change: exposing UsersHandler

--- internal/routes/users.routes.go
+++ internal/routes/users.routes.go
@@ -1,1 +1,32 @@
 package routes
+import "net/http"
+
+var UsersSubPath string = "/users"
+var UsersIdentifierPath string = "id"
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get Users"))
+}
+
+func postUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Post User"))
+}
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get User"))
+}
+func updateUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Update User"))
+}
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete User"))
+}
+
+var UsersHandlers = map[string]func(w http.ResponseWriter, r *http.Request){
+	"CGET": getUsersHandler,
+	"POST": postUserHandler,
+	"GET": getUserHandler,
+	"PUT": updateUserHandler,
+	"DELETE": deleteUserHandler,
+}
+
